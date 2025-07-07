33675f3ee522bf96eebc3530073f431158fdf2b4
Bakthi <bakthigamage@gail.com>
2025-07-07 21:46:37 +0530

    hero

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import heroImg from '../../assets/4.jpg'; 
 const Hero = () => {
-    return <section>
-        <img src={heroImg} 
-        alt="offmoon"
-        className="w-full h-[500px] md:h-[800px] lg:h-[550px] object-cover" />    
-    </section>
+    return (
+        <section className="relative">
+            <img
+                src={heroImg}
+                alt="offmoon"
+                className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] object-cover"
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+                <div className="text-center text-white p-4">
+                    <h1 className="text-[2.5rem] sm:text-[6rem] md:text-[10rem] lg:text-[15rem] xl:text-[20rem] italic font-wushin font-bold tracking-normal uppercase mb-2 drop-shadow-5xl">
+                        OFFMOON<br />
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
 }
 export default Hero;
--- src/components/Layout/Topbar.jsx
+++ src/components/Layout/Topbar.jsx
@@ -5,7 +5,7 @@ import { IoLogoTiktok } from 'react-icons/io5';
 const Topbar = () => {
     return (
         <div className="bg-[black] text-white">
-            <div className="container mx-auto flex justify-start items-center px-2 py-3 min-h-[30px]">
+            <div className="container mx-auto flex justify-between items-center px-2 py-3 min-h-[30px]">
                 <div className="flex items-center space-x-4">
                     <a href="#" className="hover:text-white text-xl">
                         <IoLogoFacebook className="h-8 w-8"/>
@@ -17,6 +17,10 @@ const Topbar = () => {
                         <IoLogoTiktok className="h-8 w-8"/>
                     </a>
                 </div>
+                <div className="flex-1 flex justify-center">
+                    <span className="text-xl font-medium tracking-wide">Wear the moon. Be the trend.</span>
+                </div>
+                <div className="w-32"></div>
             </div>
         </div>
     );
